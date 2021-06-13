2479392d93fb3252a56814b170ccd0152d3ac059
sanjitsinha19 <84683871+sanjitsinha19@users.noreply.github.com>
2021-06-13 22:01:25 +0530

    Update gdrive.py

--- userge/plugins/misc/gdrive.py
+++ userge/plugins/misc/gdrive.py
@@ -40,9 +40,9 @@ OAUTH_SCOPE = [
 ]
 REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob"
 G_DRIVE_DIR_MIME_TYPE = "application/vnd.google-apps.folder"
-G_DRIVE_FILE_LINK = "📄 <a href='https://drive.google.com/open?id={}'>{}</a> __({})__"
+G_DRIVE_FILE_LINK = "➥ 📄 <a href='https://drive.google.com/open?id={}'>{}</a> ({}) \n"
 G_DRIVE_FOLDER_LINK = (
-    "📁 <a href='https://drive.google.com/drive/folders/{}'>{}</a> __(folder)__"
+    "➥ 📁 <a href='https://drive.google.com/drive/folders/{}'>{}</a> (folder) ({}) \n"
 )
 _GDRIVE_ID = re.compile(
     r"https://drive.google.com/[\w?.&=/]+([-\w]{33}|(?<=[/=])0(?:A[-\w]{17}|B[-\w]{26}))"
